284bbeb7aec08794373680e1138ff65cbcbd91d2
alexm <alex-m@outlook.com>
2023-04-04 13:56:23 -0700

    Setup test env and import necessary modules; write beforeEach() and afterEach() functions; write test for GET /items

--- app.test.js
+++ app.test.js
@@ -2,3 +2,45 @@
 // Exercise 30.2 - Express JSON Shopping List
 
 /** Integration tests for Express JSON shopping list app. */
+
+process.env.NODE_ENV = "test";
+
+const request = require("supertest");
+
+const { app } = require("./app");
+const { items } = require("./fakeDb");
+
+
+let reeses = {
+    name: "Reese's",
+    price: 2.99
+};
+
+let fruitLoops = {
+    name: "Fruit Loops",
+    price: 4.98
+};
+
+beforeEach(() => {
+    items.length = 0;
+    items.push(reeses, fruitLoops);
+});
+
+afterEach(() => {
+    items.length = 0;
+})
+
+describe("Tests for GET /items", () => {
+
+    test("GET /items should return a list of items: [{name: <name>, price: <price>}, ...]",
+        async () => {
+            const resp = await request(app).get("/items");
+
+            expect(resp.statusCode).toEqual(200);
+            expect(resp.body).toEqual([
+                {name: "Reese's", price: 2.99},
+                {name: "Fruit Loops", price: 4.98}
+            ]);
+        })
+})
+
