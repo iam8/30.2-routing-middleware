c6fe6c5a41d806975e277ebe5d045f9241b745b7
alexm <alex-m@outlook.com>
2023-04-03 18:35:42 -0700

    Add some data to global items list

--- fakeDb.js
+++ fakeDb.js
@@ -3,6 +3,9 @@
 
 /** Storage for shopping list items. */
 
-global.items = [];
+global.items = [
+    {name: "popsicle", price: 1.45},
+    {name: "cheerios", price: 3.40},
+];
 
 module.exports = items;
