a6d1ebbb4101545edde328a71bb1fa70cb4e2414
alexm <alex-m@outlook.com>
2023-04-03 17:59:36 -0700

    Add necessary code to start Express server

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 // Ioana A Mititean
 // Exercise 30.2 - Express JSON Shopping List
+
+/** Start Express server for app. */
+
+const { app } = require("./app");
+
+
+app.listen(3000, "127.0.0.1", () => {
+    console.log("App running on 127.0.0.1, port 3000");
+})
