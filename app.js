b671c936e9e29b751ab2e3f4f246ee0a3b6a94bf
alexm <alex-m@outlook.com>
2023-04-03 18:10:56 -0700

    Set up app and module exports

--- app.js
+++ app.js
@@ -2,3 +2,10 @@
 // Exercise 30.2 - Express JSON Shopping List
 
 /** JSON API application for storing and modifying a simple shopping list. */
+
+const express = require("express");
+const app = express();
+
+module.exports = {
+    app,
+};
